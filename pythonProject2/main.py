2739b086c4da6fe9d6fb69c317e59eb3c4062989
Gim <gbks081@gmail.com>
2022-05-24 11:51:08 +0900

    feat: bio data class practice added.

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -1,16 +1,91 @@
-# This is a sample Python script.
+f = open('data.txt', 'r')
+data = f.readlines()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+data1 = 0
+mean = 0
+totalSum = 0
+totalDouble = 0
+vari = 0
+i = 0
+totalTwice = 1
+totalList = []
+
+for line in data:
+    line = line.replace('\n', '')
+    totalSum = totalSum + float(line)
+    totalDouble = totalDouble + float(line)*float(line)
+    totalTwice = totalTwice * float(line)
+    totalList.append(float(line))
+    i += 1
+
+    # data1_str = data1_str.split('\n')
+    # data1 = float(data1_str)
+    # data2 = data1_str.split('\n')
+    # data1 = int(data1_str)
+f.close()
+
+mean = totalSum / i
+vari = totalDouble / i - mean**2
+# 분산 = 제곱의평균 - 평균의제곱
+
+# median
+totalList.sort()
+if len(totalList) % 2 == 1:
+    median = totalList[int((len(totalList) - 1) / 2)]
+else:
+    median = totalList[len(totalList) / 2]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# Mean, variance, and standard deviation
+print('---------------------')
+print('totalSum:', totalSum)
+print('totalDouble:', totalDouble)
+print('totalTwice:', totalTwice)
+print('mean:', mean)
+print('variance(s^2):', vari)
+print('std devi:', vari**(1/2))
+print('geometric mean:', totalTwice**(1/i))
+print("median = " + str(median))
+
+# data2 = list(data1)
+# print(data1.split('\n'))
+# print(int(data2))
+# print(data2)
+# print(sum(data2))
+# print(data1)
+# print(type(line))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# def mean() :
+#     sum(data1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print('mean:',sum(data1))
+
+
+
+
+
+
+
+
+
+
+
+
+# # This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
