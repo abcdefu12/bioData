f4348be2c069a2e979b17a2b5069c7aa60ce246e
Gim <gbks081@gmail.com>
2022-05-25 14:13:55 +0900

    feat: result of translation which was given
    e.coli DNA seq

--- centralDogma/main.py
+++ centralDogma/main.py
@@ -2,7 +2,6 @@
 print('<< translation of central Dogma >>')
 
 # codon table file read
-
 aaSeq1char = {}  # aa name: single-letter ex.A
 aaSeq3char = {}  # aa name: three-letter ex.Ala
 
@@ -25,4 +24,25 @@ f.close()
 print('< codon Table >')
 print('codon Table for 1Char:', aaSeq1char)
 print('codon Table for 3Char:', aaSeq3char)
+# print('--------------------------------------')
+
+
+# Get DNA sequence which was provided
+f = open('dna_sequence.txt', 'r')   # now it is e.coli DNA sequence
+sequence = ''
+for line in f.readlines():
+    sequence = sequence + line.strip().upper().replace(' ', '')
+f.close()
+
+proteinSeq1Char = ''
+proteinSeq3Char = ''
+
+for i in range(0, len(sequence), 3):
+    Triplet = sequence[i:i + 3]
+    proteinSeq1Char = proteinSeq1Char + aaSeq1char[Triplet]
+    proteinSeq3Char = proteinSeq3Char + aaSeq3char[Triplet]
+
+print('< result of translation >')
+print('protein seq of single letter:', proteinSeq1Char)
+print('protein seq of triple letter:', proteinSeq3Char)
 print('--------------------------------------')
